24108d5cfe7622317e6f3469b40f7bd83fcb54a8
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-14 15:51:02 -0700

    JS for random numbers

---
+++ index.js
@@ -0,0 +1,23 @@
+const cardOneContainer = document.getElementById("card-1");
+const cardTwoContainer = document.getElementById("card-2");
+const cardThreeContainer = document.getElementById("card-3");
+const spin = document.getElementById("btn");
+
+function randomNumber(){
+    const ranNum1 = Math.floor(Math.random() * 10) +1;
+    const ranNum2 = Math.floor(Math.random() * 10) +1;
+    const ranNum3 = Math.floor(Math.random() * 10) +1;
+
+    cardOneContainer.textContent = ranNum1;
+    cardTwoContainer.textContent = ranNum2;
+    cardThreeContainer.textContent = ranNum3;
+
+    cardOneContainer.style.fontSize= "150px";
+    cardTwoContainer.style.fontSize= "150px"
+    cardThreeContainer.style.fontSize= "150px"
+
+}
+
+spin.addEventListener("click", randomNumber);
+
+randomNumber();
