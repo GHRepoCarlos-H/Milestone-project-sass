9c3e5ff3f077c820ada227a02c850ce15749bad7
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-20 20:29:53 -0700

    reset button not working

--- result.js
+++ result.js
@@ -5,8 +5,8 @@ const resultContainer = document.getElementById("result-card");
 /*resultContainer.textContent = (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent) ? "You're a Jackpot Winner!!" : "Try Again!!";*/
 function winnerCheck(){
 if(cardOneDiv.textContent == cardTwoDiv.textContent && cardTwoDiv.textContent == cardThreeDiv.textContent){
-    resultContainer.textContent = ("Jackpot")
-    const winnerPageURL = "winnerPage.html"
+    resultContainer.textContent = ("Jackpot");
+    const winnerPageURL = "winnerPage.html";
     window.open(winnerPageURL, '_blank');
     spin.disabled = true;
 } 
@@ -21,13 +21,15 @@ return resultContainer.textContent;
 //Close winning screen and reset the page. W3Schools for reference (Window close();)
 
 const gameReset = document.getElementById("reset-button");
-let winnerPageWindow;
+var winnerPageWindow;
 
 function resetScreen(){
     if (winnerPageWindow) {
         winnerPageWindow.close();
         
     }
+    window.location.reload();
 }
 
 gameReset.addEventListener("click", resetScreen);
+
