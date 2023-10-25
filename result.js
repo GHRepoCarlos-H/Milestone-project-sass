7e3db99337c122cdb25103a022a648750b46cbdf
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-24 20:13:42 -0700

    Fixed console error. <script> file issues.

--- result.js
+++ result.js
@@ -5,23 +5,23 @@ resultContainer.textContent = ("Press SPIN button!!");
 
 /*resultContainer.textContent = (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent) ? "You're a Jackpot Winner!!" : "Try Again!!";*/
 function winnerCheck(){
-if(cardOneDiv.textContent == cardTwoDiv.textContent && cardTwoDiv.textContent == cardThreeDiv.textContent){
-    resultContainer.textContent = ("Jackpot");
-    const winnerPageURL = "winnerPage.html";
-    const winWindow = window.open(winnerPageURL, '_blank');
-    spin.disabled = true;
+    if(cardOneDiv.textContent == cardTwoDiv.textContent && cardTwoDiv.textContent == cardThreeDiv.textContent){
+        resultContainer.textContent = ("Jackpot");
+        const winnerPageURL = "winnerPage.html";
+        const winWindow = window.open(winnerPageURL, '_blank');
+        spin.disabled = true;
 
-    setTimeout(function() {
-        winWindow.close();
-        spin.disabled = false;
-    }, 6000);
+        setTimeout(function() {
+            winWindow.close();
+            spin.disabled = false;
+        }, 6000);
 
-    addCredits();
+        addCredits();
 
-} 
-else{
-    resultContainer.textContent = ("Try again!")
-}
+    } 
+    else{
+        resultContainer.textContent = ("Try again!")
+    }
 
 }
 
