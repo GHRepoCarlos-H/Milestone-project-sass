2162c61d01247c60a6056e5c66213a78367e0753
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-07 19:47:00 -0800

    Completed code

--- result.js
+++ result.js
@@ -1,30 +1,59 @@
 
 const resultContainer = document.getElementById("result-card");
-resultContainer.textContent = ("Press SPIN button!!");
+resultContainer.textContent = ("PLACE YOUR BET ");
 
 
-function winnerCheck(){
-    if(cardOneDiv.textContent == cardTwoDiv.textContent && cardTwoDiv.textContent == cardThreeDiv.textContent){
-        resultContainer.textContent = ("Jackpot");
-        const winnerPageURL = "winnerPage.html";
-        const winWindow = window.open(winnerPageURL, '_blank');
-        spin.disabled = true;
+function winnerCheck() {
+    if (cardOneDiv.textContent === cardTwoDiv.textContent && cardTwoDiv.textContent === cardThreeDiv.textContent) {
+        if (cardOneDiv.textContent === 7) {
+            resultContainer.textContent = "Jackpot";
+            const winnerPageURL = "winnerPage.html";
+            const winWindow = window.open(winnerPageURL, '_blank');
+            currentScore *= 10;
+            document.getElementById("jackpotSound").play();
+            disableSpin();
 
+            setTimeout(function () {
+                winWindow.close();
+                enableSpin();
+            }, 6000);
+
+        } else {
+            currentScore += betAmount * 6;
+            document.getElementById("winnerX6Sound").play();
+            resultContainer.textContent = "You Won X6";
+            disableSpin();
+
+            setTimeout(function() {
+                enableSpin();
+            }, 4000);
+        }
+    } else if (cardOneDiv.textContent === cardThreeDiv.textContent) {
+        currentScore += betAmount * 4;
+        document.getElementById("winnerX4Sound").play();
+        resultContainer.textContent = "You Won X4";
+        disableSpin();
         setTimeout(function() {
-            winWindow.close();
-            spin.disabled = false;
-        }, 6000);
-
-        addCredits();
-
-    } 
-    else{
-        resultContainer.textContent = ("Try again!")
+            enableSpin();
+        }, 1000);
+    } else {
+        resultContainer.textContent = "Try again!";
     }
 
+    updateCreditScreen();
+};
+
+
+
+function disableSpin(){
+    spin1.disabled = true;
+    spin2.disabled = true;
+    spin3.disabled = true;
 }
 
-function addCredits(){
-    currentScore *=2;
-    updateCreditScreen();
+function enableSpin(){
+    spin1.disabled = false;
+    spin2.disabled = false;
+    spin3.disabled = false;
+
 }
--- score.js
+++ score.js
@@ -2,33 +2,70 @@
 let currentScore = 100;
 
 
-spin.addEventListener("click", function(){
-    winnerCheck();
-    checkAvailableCredit();
+function addSpinClickEvent(button) {
+    button.addEventListener('click', function(){
+        document.getElementById("buttonClickSound").play();
+        randomNumber();
+        setBetAmount();
+        winnerCheck();
+    });
+};
 
-});
+addSpinClickEvent(spin1);
+addSpinClickEvent(spin2);
+addSpinClickEvent(spin3);
 
-function checkAvailableCredit(){
-    if(currentScore > 1) {
+
+
+let betAmount = 0;
+
+function setBetAmount(buttonId){
+    if(buttonId === "btn1"){
+        betAmount = 1;
+    } else if (buttonId === "btn2"){
+        betAmount = 5;
+    } else if (buttonId === "btn3"){
+        betAmount = 10;
+    }
+}
+
+
+const buttons = document.querySelectorAll(".button-class");
+
+buttons.forEach(button => {
+button.addEventListener("click", function() {
+    if(button.id === "btn1" && currentScore >= 2) {
         currentScore -= 1; 
-        updateCreditScreen();
- 
-    }else {
+        setBetAmount("btn1");
+
+    }else if (button.id === "btn2" && currentScore >= 6) {
+        currentScore -= 5
+        setBetAmount("btn2");
+
+    }else if (button.id === "btn3" && currentScore >= 11) {
+        currentScore -= 10;
+        setBetAmount("btn3");
+
+    }else{
         const playAgainURL = "playAgain.html";
         const newWindow =  window.open(playAgainURL, '_blank')
+        document.getElementById("You-lose-sound").play();
 
         setTimeout (function() {
         newWindow.close();
         location.reload();
-         }, 2000);
+         }, 3000);
          
     }
     
-    return currentScore;
-}
+    updateCreditScreen();
+    });
+});
+
+
 
 function updateCreditScreen() {
     document.getElementById("score-card").textContent = "Credit: " + "$" + currentScore;
 }
 
-updateCreditScreen(); 
+updateCreditScreen();
